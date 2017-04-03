24f426c332d006dec0cda4d466182072aa9e4688
Raymond Menzel <Raymond.Menzel@noaa.gov>
2017-04-03 11:11:50 -0400

    updated gitignore, fixed bug in cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,145 +1,135 @@
-#!/bin/tcsh -f
+#!/bin/bash -f
 
 #Clean up after the test suite is run.
 
-#Handle command line arguments.
-if ($#argv == 0) then
-    echo "No arguments given, using default values."
-else
-    echo "Error: No command line arguments supported."
-    exit
-endif
-
-#Print out starting test suite message.
 echo "Cleaning up ..."
 
 #Change into the build directory.
 cd build
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: build directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Get rid of any old GRTcode binaries and executables.
 echo "Removing old GRTcode binary files and executables ..."
 make clean
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: make clean for grtcode.x failed."
     exit 1
-endif
+fi
 
 #Change to the run directory.
 cd ../run
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: run directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Remove the executable from the run directory.
-rm grtcode.x
-if ($?) then
+rm -f grtcode.x
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove grtcode.x failed."
     exit 1
-endif
+fi
 
 #Change to the RESULTS directory.
 cd RESULTS
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: RESULTS directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Remove results files from the RESULTS directory.
-rm *.nc
-if ($?) then
+rm -f *.nc
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove resulting files failed."
     exit 1
-endif
+fi
 
 #Change to the verification directory.
 cd ../verification
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: verification directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Change into the build directory.
 cd build
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: build directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Get rid of any old verification binaries and executables.
 echo "Removing old verification binary files and executables ..."
 make clean
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: make clean failed for verification.x."
     exit 1
-endif
+fi
 
 #Change to the run directory.
 cd ../run
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: run directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Remove the executable from the run directory.
-rm verification.x
-if ($?) then
+rm -f verification.x
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove grtcode.x failed."
     exit 1
-endif
+fi
 
 #Change to the RESULTS directory.
 cd RESULTS
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: RESULTS directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Remove results files from the RESULTS directory.
-rm *.nc.*
-if ($?) then
+rm -f *.nc.*
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove resulting files failed."
     exit 1
-endif
+fi
 
 #Change to the plots directory.
 cd ../../plots
-if ($?) then
+if [ $? -ne 0 ]; then
     echo "Error: plots directory does not exist or path is wrong."
     exit 1
-endif
+fi
 
 #Remove .gnuplot files.
-rm *.gnuplot
-if ($?) then
+rm -f *.gnuplot
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove .gnuplot files failed."
     exit 1
-endif
+fi
 
 #Remove *errors_above* files.
-rm *errors_above*
-if ($?) then
+rm -f *errors_above*
+if [ $? -ne 0]; then
     echo "Error: attempt to remove *errors_above* files failed."
     exit 1
-endif
+fi
 
 #Remove .png files.
-rm *.png
-if ($?) then
+rm -f *.png
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove .png files failed."
     exit 1
-endif
+fi
 
 #Remove .gif files.
-rm *.gif
-if ($?) then
+rm -f *.gif
+if [ $? -ne 0 ]; then
     echo "Error: attempt to remove .gif files failed."
     exit 1
-endif
+fi
 
-#Print all done.
 echo "All done."
