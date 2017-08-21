950f1dad53973450b2c71b71897c801ff9806c5c
Raymond Menzel <Raymond.Menzel@noaa.gov>
2017-08-21 08:30:00 -0400

    whitespace

--- src/continuum.c
+++ src/continuum.c
@@ -80,64 +80,67 @@ void parseCKD(const char fname[],
 
 }
 
-/* void ctm_free(REAL_t** CS, */
-/*               REAL_t** CF, */
-/*               REAL_t** T0, */
-/*               REAL_t** T0F)     */
-/* { */
-/*   free(*CS); */
-/*   *CS = NULL; */
-/*   free(*CF); */
-/*   *CF = NULL; */
-/*   free(*T0); */
-/*   *T0 = NULL; */
-/*   free(*T0F); */
-/*   *T0F = NULL;   */
-/* } */
-
-void ctm_read(REAL_t** CS,
-              REAL_t** CF,
-              REAL_t** T0,
-              REAL_t** T0F,
-              int maxwavenum)    
+void ctm_read(REAL_t **CS,
+              REAL_t **CF,
+              REAL_t **T0,
+              REAL_t **T0F,
+              int maxwavenum)
 {
-  const char testCKDF_f[] = "continuum/CKDF.ppp";            /* T0F */
-  const char testCKDS_f[] = "continuum/CKDS.ppp";            /* T0  */
-  const char testMTCKDF_f[] = "continuum/296MTCKD25_F.ccf";  /* CF  */
-  const char testMTCKDS_f[] = "continuum/296MTCKD25_S.ccf";  /* CS  */
+    const char testCKDF_f[] = "continuum/CKDF.ppp";            /* T0F */
+    const char testCKDS_f[] = "continuum/CKDS.ppp";            /* T0  */
+    const char testMTCKDF_f[] = "continuum/296MTCKD25_F.ccf";  /* CF  */
+    const char testMTCKDS_f[] = "continuum/296MTCKD25_S.ccf";  /* CS  */
 
-  parseCKD(testCKDF_f, T0F, maxwavenum);
-  parseCKD(testCKDS_f, T0, maxwavenum);
-  parseCKD(testMTCKDF_f, CF, maxwavenum);
-  parseCKD(testMTCKDS_f, CS, maxwavenum);
-  
-  return;
+    parseCKD(testCKDF_f,
+             T0F,
+             maxwavenum);
+    parseCKD(testCKDS_f,
+             T0,
+             maxwavenum);
+    parseCKD(testMTCKDF_f,
+             CF,
+             maxwavenum);
+    parseCKD(testMTCKDS_f,
+             CS,
+             maxwavenum);
 }
 
-void get_PD(REAL_t* const PD, REAL_t const * const P, REAL_t const * const H2O, const size_t nlyr)
+void get_PD(REAL_t * const PD,
+            REAL_t const * const P,
+            REAL_t const * const H2O,
+            const size_t nlyr)
 {
-  unsigned int n;
-  for(n = 0; n < nlyr ; ++n)
-  {
-    /* PD[n] = ((REAL_t)100.) * H2O[n] * P[n]/((REAL_t)1E6); original was written for P in mb */
-    PD[n] = ((REAL_t)100.) * H2O[n] * (1013.25*P[n])/((REAL_t)1E6);
-  }
+    /*Local variables*/
+    unsigned int n;
+
+    for(n = 0; n < nlyr ; ++n)
+    {
+/*
+        PD[n] = ((REAL_t)100.)*H2O[n]*P[n]/((REAL_t)1E6); original was written for P in mb
+*/
+        PD[n] = ((REAL_t)100.)*H2O[n]*(1013.25*P[n])/((REAL_t)1E6);
+    }
 }
 
-
-void get_PF(REAL_t* const  PF, REAL_t const * const P, REAL_t const * const PD, const size_t nlyr)
+void get_PF(REAL_t * const PF,
+            REAL_t const * const P,
+            REAL_t const * const PD,
+            const size_t nlyr)
 {
-  unsigned int n;
-  for(n = 0; n < nlyr ; ++n)
-  {
-    /* PF[n] = ((REAL_t)100.) * P[n] - PD[n];  original was written for P in mb */
-    PF[n] = ((REAL_t)100.) * (1013.25*P[n]) - PD[n];
-  }
+    /*Local variables*/
+    unsigned int n;
+
+    for(n=0;n<nlyr;++n)
+    {
+/*
+        PF[n] = ((REAL_t)100.)*P[n] - PD[n];  original was written for P in mb
+*/
+        PF[n] = ((REAL_t)100.)*(1013.25*P[n]) - PD[n];
+    }
 }
 
-
-void get_TF_TD(REAL_t* const TF,  /* out */
-               REAL_t* const TD,  /* out */
+void get_TF_TD(REAL_t * const TF,
+               REAL_t * const TD,
                REAL_t const * const PD,  /* from get_PD, requres P, H2O, N */
                REAL_t const * const PF,  /* from get_PF, requires P, PD, N */
                REAL_t const * const CS,  /* ctm read */
@@ -148,31 +151,32 @@ void get_TF_TD(REAL_t* const TF,  /* out */
                REAL_t const * const T0F, /* ctm read */
                const size_t nlines,
                const size_t nlyr)
-{  
-  unsigned int lyr;
-  unsigned int lp;
-  const REAL_t c1 = 1E3;
-  const REAL_t c2 = 101325.;
-  const REAL_t c3 = 1.3806E-19;
-  const REAL_t ct = 296.0;
-  
-  for(lyr = 0; lyr < nlyr; ++lyr)
-  {
-    for(lp = 0; lp < nlines; ++lp)
-    {      
-      TF[lyr*nlines + lp ] = ( PD[lyr] * PF[lyr] * CF[lp] * HGT[lyr] * c1 /
-                  (c2 * c3 * T[lyr] ) ) *
-          ( ct / T[lyr] ) * exp( T0F[lp] * (ct - T[lyr]) ) ;
-      
-      TD[lyr*nlines + lp ] = ( (PD[lyr]*PD[lyr]) * CS[lp] * HGT[lyr] * c1 /
-                  (c2 * c3 * T[lyr] ) ) *
-          ( ct / T[lyr] ) * exp( T0[lp] * (ct - T[lyr]) ) ;              
-    }  /* for line */
-  }    /* for layer */
-}
-            
+{
+    /*Local variables*/
+    unsigned int lyr;
+    unsigned int lp;
+    const REAL_t c1 = 1E3;
+    const REAL_t c2 = 101325.;
+    const REAL_t c3 = 1.3806E-19;
+    const REAL_t ct = 296.0;
 
-void get_CTM(REAL_t* const OPT_CTM,
+    for (lyr=0;lyr<nlyr;++lyr)
+    {
+        for(lp=0;lp<nlines;++lp)
+        {
+            TF[lyr*nlines+lp] = (PD[lyr]*PF[lyr]*CF[lp]*HGT[lyr]*c1/
+                                (c2*c3*T[lyr]))*
+                                (ct/T[lyr])*exp(T0F[lp]*(ct-T[lyr]));
+
+            TD[lyr*nlines+lp] = ((PD[lyr]*PD[lyr])*CS[lp]*HGT[lyr]*c1/
+                                (c2*c3*T[lyr]))*
+                                (ct/T[lyr])*exp(T0[lp]*(ct-T[lyr]));
+        }
+    }
+}
+
+/*Calculate the optical depths associated with the continuum.*/
+void get_CTM(REAL_t * const OPT_CTM,
              REAL_t const * const T,
              REAL_t const * const P,
              REAL_t const * const HGT,
@@ -180,90 +184,92 @@ void get_CTM(REAL_t* const OPT_CTM,
              const size_t NLINES,
              const size_t NLAYERS)
 {
-  unsigned int n;
-  REAL_t* CS = NULL;
-  REAL_t* CF = NULL;
-  REAL_t* T0 = NULL;
-  REAL_t* T0F = NULL;
-  REAL_t* IBUF = NULL;
-  REAL_t* PD = NULL;
-  REAL_t* PF = NULL;
-  REAL_t* PBUF = NULL;
-  REAL_t* TD = NULL;
-  REAL_t* TF = NULL;
-  REAL_t* TBUF = NULL;
+    /*Local variables*/
+    unsigned int n;
+    REAL_t *CS = NULL;
+    REAL_t *CF = NULL;
+    REAL_t *T0 = NULL;
+    REAL_t *T0F = NULL;
+    REAL_t *IBUF = NULL;
+    REAL_t *PD = NULL;
+    REAL_t *PF = NULL;
+    REAL_t *PBUF = NULL;
+    REAL_t *TD = NULL;
+    REAL_t *TF = NULL;
+    REAL_t *TBUF = NULL;
 
-  /* later for gpu improvement,
-   * we almost certainly want this to be array of float4,
-   * not 4 arrays of floats... */
-  
-  IBUF = (REAL_t*)calloc(4*NLINES,sizeof(REAL_t));
-  if(IBUF == NULL)
-  {
-    fprintf(stderr,"IBUF malloc failed");
-    exit(EXIT_FAILURE);
-  }
-  CS = &(IBUF[0]);
-  CF = &(IBUF[1*NLINES]);
-  T0 = &(IBUF[2*NLINES]);
-  T0F = &(IBUF[3*NLINES]);
+    IBUF = (REAL_t *)calloc(4*NLINES,
+                            sizeof(REAL_t));
+    if (IBUF == NULL)
+    {
+        fprintf(stderr,
+                "IBUF malloc failed.");
+        exit(EXIT_FAILURE);
+    }
+    CS = &(IBUF[0]);
+    CF = &(IBUF[1*NLINES]);
+    T0 = &(IBUF[2*NLINES]);
+    T0F = &(IBUF[3*NLINES]);
 
-  PBUF = (REAL_t*)calloc(2*NLAYERS,sizeof(REAL_t));
-  if(PBUF == NULL)
-  {
-    fprintf(stderr,"PBUF malloc failed");
-    exit(EXIT_FAILURE);
-  }
-  PD = &(PBUF[0]);
-  PF = &(PBUF[NLAYERS]);
+    PBUF = (REAL_t *)calloc(2*NLAYERS,
+                            sizeof(REAL_t));
+    if (PBUF == NULL)
+    {
+        fprintf(stderr,
+                "PBUF malloc failed.");
+        exit(EXIT_FAILURE);
+    }
+    PD = &(PBUF[0]);
+    PF = &(PBUF[NLAYERS]);
 
-  TBUF = (REAL_t*)calloc(2*NLINES*NLAYERS,sizeof(REAL_t));
-  if(TBUF == NULL)
-  {
-    fprintf(stderr,"TBUF malloc failed");
-    exit(EXIT_FAILURE);
-  }
-  TF = &(TBUF[0]);
-  TD = &(TBUF[NLINES*NLAYERS]);
-  
-  ctm_read(&CS, &CF, &T0, &T0F, NLINES);
+    TBUF = (REAL_t *)calloc(2*NLINES*NLAYERS,
+                            sizeof(REAL_t));
+    if (TBUF == NULL)
+    {
+        fprintf(stderr,
+                "TBUF malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    TF = &(TBUF[0]);
+    TD = &(TBUF[NLINES*NLAYERS]);
 
-  /* printf("%g %g %g\n", H2O[0],H2O[1],H2O[2]); */
-  get_PD( PD, P, H2O, NLAYERS);
-  get_PF( PF, P, PD, NLAYERS);
-  /* for( n = 0; n < NLAYERS ; ++n ) */
-  /* { */
-  /*   printf("%g %g\n", PD[n], PF[n]);   */
-  /* } */
+    ctm_read(&CS,
+             &CF,
+             &T0,
+             &T0F,
+             NLINES);
 
-  
-  get_TF_TD( TF,  /* out */
-             TD,  /* out */
-             PD,  /* from get_PD, requres P, H2O, NLAYER */
-             PF,  /* from get_PF, requires P, PD, NLAYER */
-             CS,  /* ctm read */
-             CF,  /* ctm read */
-             HGT, /* model layer height*/
-             T,   /* model temp */
-             T0,  /* ctm read */
-             T0F, /* ctm read */
-             NLINES,
-             NLAYERS);
+    get_PD(PD,
+           P,
+           H2O,
+           NLAYERS);
+    get_PF(PF,
+           P,
+           PD,
+           NLAYERS);
 
-  for( n = 0; n < NLINES*NLAYERS ; ++n )
-  {
-    /* sum self and foreign */
-    OPT_CTM[n] += TF[n] + TD[n];
-    /* /\* debug print *\/ */
-    /* printf("DBG CTM: %g \n",TF[n] + TD[n]); */
-  }
-  
-  free(IBUF);
-  free(TBUF);
-  free(PBUF);
-  
+    get_TF_TD(TF,  /* out */
+              TD,  /* out */
+              PD,  /* from get_PD, requres P, H2O, NLAYER */
+              PF,  /* from get_PF, requires P, PD, NLAYER */
+              CS,  /* ctm read */
+              CF,  /* ctm read */
+              HGT, /* model layer height*/
+              T,   /* model temp */
+              T0,  /* ctm read */
+              T0F, /* ctm read */
+              NLINES,
+              NLAYERS);
+
+    for(n=0;n<NLINES*NLAYERS;++n)
+    {
+        OPT_CTM[n] += TF[n] + TD[n];
+    }
+
+    free(IBUF);
+    free(TBUF);
+    free(PBUF);
 }
-  
 
 #ifndef SKIPMAIN
 /* tests */
