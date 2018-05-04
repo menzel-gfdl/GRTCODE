259fdf5b555cd874bcb30f8386c2a0fd71edd113
Raymond Menzel <Raymond.Menzel@noaa.gov>
2018-05-04 18:17:33 -0400

    runs sw on gpu

--- device.sh
+++ device.sh
@@ -7,7 +7,7 @@ MAKEFILE="Makefile.nvcc"
 
 cd packages/radiation_solvers
 make -f $MAKEFILE clean
-make -f $MAKEFILE -j4
+make -f $MAKEFILE -j4 CFLAGS="-O3"
 if [ $? -ne 0 ]; then
     printf "Radiation_solvers library build failed.\n"
     exit 1
@@ -23,5 +23,6 @@ fi
 
 mv $GRT $RUN
 cd $RUN
-time ./$GRT -aINPUT/new.multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-0-3.0_none.nc -r1.0 -ofoo --h2octm --o3ctm -t0 -T0 -x0 -X0 -y0 -Y0 -w1 -W50000 \
-    HITFILES/01_hit12.par HITFILES/02_hit12.par HITFILES/03_hit12.par HITFILES/04_hit08.par HITFILES/06_hit12.par HITFILES/07_hit12.par
+time ./$GRT -aINPUT/new.multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-0-3.0_none.nc -ofoo --h2octm --o3ctm \
+HITFILES/01_hit12.par HITFILES/02_hit12.par HITFILES/03_hit12.par HITFILES/04_hit08.par HITFILES/06_hit12.par HITFILES/07_hit12.par \
+-r1.0 -w1 -W50000
