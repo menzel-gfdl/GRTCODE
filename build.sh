43a2e805246db28f3c82ae43b352fd7599d86038
Raymond Menzel <Raymond.Menzel@noaa.gov>
2022-11-02 17:20:16 -0400

    make the build script more flexible

--- build.sh
+++ build.sh
@@ -1,11 +1,43 @@
 #!/bin/bash -e
 
-source gpubox.env.sh
-HERE=$PWD
+# Handle command line arguments.
+arglist="$0 [-h|--help] env [-d]"
+counter=0
+while [[ $# -gt 0 ]]; do
+  key=$1
+  case $key in
+    -h|--help)
+      echo "$arglist"
+      echo "\nPositional arguments:"
+      echo "env:             Environment script to source."
+      echo "\nOptional arguments:"
+      echo "-d:              Install dependencies."
+      echo "-h, --help:      Print this help message."
+      exit 0
+    ;;
+    -d)
+      install_dependencies="yes"
+      shift
+    ;;
+    *)
+      counter=$((counter+1))
+      env="$key"
+      shift
+    ;;
+  esac
+done
+if [ "$counter" -ne "1" ]; then
+  echo "Error: incorrect number of arguments."
+  echo "usage: $arglist"
+  exit 1
+fi
 
-# Install hdf5
-#INSTALL_DEPENDENCIES="yes"
-if [ -n "$INSTALL_DEPENDENCIES" ]; then
+# Source environment.
+source $env
+here="$PWD"
+
+if [ -n "$install_dependencies" ]; then
+  # Install hdf5
   cd $PREFIX
   if [ ! -d "hdf5" ]; then
     git clone https://github.com/HDFGroup/hdf5.git
@@ -32,13 +64,14 @@ else
   echo "Dependencies are assumed to be installed in ${PREFIX}/lib"
 fi
 
-# Build the code.
-cd "$HERE"
+# Build the GPU-capable version.
+cd "$here"
 mkdir -p bld-gpu
 cd bld-gpu
 CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" NVCC="${NVCC}" NVCCFLAGS="${NVCCFLAGS}" NVCCLDFLAGS="${NVCCLDFLAGS}" MPICC="${MPICC}" MPICXX="${MPICXX}" RPATH="${RPATH}" make -f ../Makefile-gpu
 
-cd "$HERE"
+# Build the host-only version.
+cd "$here"
 mkdir -p bld-host
 cd bld-host
 CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" MPICC="${MPICC}" MPICXX="${MPICXX}" RPATH="${RPATH}" make -f ../Makefile-host
